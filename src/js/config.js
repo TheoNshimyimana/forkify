bfd90ab3e4d4fe4efd3adb5ade84a1e3cae9b240
xetheo <xetheo7@gmail.com>
2023-06-10 12:43:35 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'aedc0162-958a-48eb-8c42-23e55df01c19';
+export const MODAL_CLOSE_SEC = 2.5;
